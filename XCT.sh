c317f23a43093ef011b0bf0c2758cf150cb68948
eggplantredrage <54252049+eggplantredrage@users.noreply.github.com>
2021-10-21 15:44:12 -0400

    Update XCT.sh
    October 21 2021 Ver 3 
    Release Notes 
    Replace the Exit key to the q key 
    added a packages list command 
    added a Package upgrade option

--- XCT.sh
+++ XCT.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-echo "================================"
+echo "======================================="
 echo "XCT XOS Command Tool "
-echo "========MENU====================="
+echo "========MENU==========================="
 echo "Press 1 to update grub"
 echo "Press 2 to update Repo's"
 echo "Press 3 to repair broken Packages"
 echo "Press 4 to see the Linux Version"
-echo "Press 5 to Exit"
-echo "================================"
+echo "Press 5 to see the Packages Installed"
+echo "Press 6 to Upgrade the Packages"
+echo "Press Q to Exit"
+echo "======================================"
 echo -e "\n"
 echo -e "\n"
 echo -e "Enter Your Option \c "
@@ -18,6 +20,7 @@ case "$answer" in
 3) sudo apt --fix-missing update
    sudo apt-get clean ;;
 4) uname -r ;;
-5) exit ;;
+5) command apt list;;
+6) sudo apt-get upgrade ;;
+Q) exit ;;
 esac
-
