c0462f4f7fdc644f05e0d0648d25d4c0d7c07242
eggplantredrage <54252049+eggplantredrage@users.noreply.github.com>
2021-11-23 21:54:38 -0500

    Update XCT.sh
    Added a nvidia xconfig option to the menu 
    Change the about and exit option to 7 and 0

--- XCT.sh
+++ XCT.sh
@@ -1,15 +1,16 @@
 #!/bin/bash
 echo "======================================="
 echo "XCT XOS Command Tool "
-echo "========MENU==========================="
+echo "========MENU=========================="
 echo "Press 1 to update grub"
 echo "Press 2 to update Repo's"
 echo "Press 3 to repair broken Packages"
 echo "Press 4 to see the Linux Version"
 echo "Press 5 to see the Packages Installed"
-echo "Press 6 to Upgrade the Packages"
-echo "Press Q to Exit"
-echo "======================================"
+echo "Press 6 to configure xconfig For Nvidia Users"
+echo "Press 7 For the About Screen"
+echo "Press 0 to Exit"
+echo "====================================="
 echo -e "\n"
 echo -e "\n"
 echo -e "Enter Your Option \c "
@@ -21,6 +22,7 @@ case "$answer" in
    sudo apt-get clean ;;
 4) uname -r ;;
 5) command apt list;;
-6) sudo apt-get upgrade ;;
-Q) exit ;;
+6) sudo nvidia-xconfig ;; 
+7) echo "XCT 2021 By Eggplant48 v00.1.0 ______________________" ;;
+0) exit ;;
 esac
